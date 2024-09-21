59d31fe2158a338a89d66bcb6d87f559051b81bc
VKAmpah <valentine.ampah@keyin.com>
2024-09-21 02:29:38 -0230

    Updates

---
+++ assets/index.js
@@ -0,0 +1,130 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import logoblack from "./logoblack.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png";
+import aws from "./tech/aws.png";
+import azure from "./tech/azure.png";
+import gcp from "./tech/gcp.png";
+import java from "./tech/java.png";
+import spark from "./tech/spark.png";
+import sql from "./tech/sql.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import awscloudfoundations from "./awscloudfoundations.png";
+import awscloudarchitecting from "./awscloudarchitecting.png";
+import awsclouddev from "./awsclouddev.png";
+import streamfy from "./streamfy.png";
+import swipe from "./swipe.png";
+import onlinestore from "./onlinestore.png";
+import medium from "./medium.svg";
+import linkedin from "./linkedin.svg";
+import github2 from "./github2.svg";
+import profile from "./profile.png";
+import profile2 from "./profile2.png";  
+import url from "./url.png";
+
+import logocolor from "./png/logocolor.png";
+import logonobackground from "./png/logonobackground.png";
+
+import cloud from "./cloud.png";
+import data from "./data.png";
+import fullstack from "./fullstack.png";
+import reub from "./reub.jpg";    
+
+import etl_airflow from "./etl_airflow.png";
+import airflow from "./airflow.png";
+import pyproject from "./pyproject.png";
+import search from "./search.png";
+import erd from "./erd.jpeg";
+import nanaB from "./nanaB.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  logoblack,
+  awscloudfoundations,
+  awscloudarchitecting,
+  awsclouddev,
+  streamfy,
+  swipe,
+  onlinestore,
+  medium,
+  linkedin,
+  github2,
+  python,
+  aws,
+  azure,
+  gcp,
+  java,
+  spark,
+  sql,
+  profile,
+  profile2,
+  url,
+  logocolor,
+  logonobackground,
+  cloud,
+  data,
+  fullstack,
+  reub,
+  etl_airflow,
+  airflow,
+  pyproject,
+  search,
+  erd,
+  nanaB,
+
+};
